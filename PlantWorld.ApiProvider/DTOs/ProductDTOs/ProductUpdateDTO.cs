7e382b4883564171ac4199ec618cb602cb995e06
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-10 17:48:28 +0200

    Create Product DTOs

---
+++ PlantWorld.ApiProvider/DTOs/ProductDTOs/ProductUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace PlantWorld.ApiProvider.DTOs.ProductDTOs
+{
+    public class ProductUpdateDTO
+    {
+        public string Name { get; set; }
+        public string? ImgUrl { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
