7e9c3aa536fe3ac79923d7489165a56d9abfc145
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-10 17:35:46 +0200

    Add Category Interface&Repository

---
+++ PlantWorld.ApiProvider/Repositories/Implementations/CategoryRepository.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlantWorld.ApiProvider.Models;
+using PlantWorld.ApiProvider.Repositories.Interfaces;
+
+namespace PlantWorld.ApiProvider.Repositories.Implementations
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .ToListAsync();
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            return await _context.Categories
+                .Include(c => c.Products)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Category> AddAsync(Category category)
+        {
+            _context.Categories.Add(category);
+            await _context.SaveChangesAsync();
+            return category;
+        }
+
+        public async Task<Category?> UpdateAsync(Category category)
+        {
+            var existingCategory = await _context.Categories.FindAsync(category.Id);
+            if (existingCategory == null)
+            {
+                return null;
+            }
+            existingCategory.Name = category.Name;
+            existingCategory.ImgUrl = category.ImgUrl;
+            existingCategory.Description = category.Description;
+            await _context.SaveChangesAsync();
+            return existingCategory;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return false;
+            }
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+
+
+}
---
+++ PlantWorld.ApiProvider/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using PlantWorld.ApiProvider.Models;
+
+namespace PlantWorld.ApiProvider.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category> AddAsync(Category category);
+        Task<Category?> UpdateAsync(Category category);
+        Task<bool> DeleteAsync(int id);
+    }
+}
