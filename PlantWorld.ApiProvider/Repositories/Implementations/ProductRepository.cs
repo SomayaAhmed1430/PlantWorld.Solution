b6c2492d3a19ed69f5e034fccddb4ae5a3566fe8
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-10 17:45:17 +0200

    Add Product Interface&Repository

---
+++ PlantWorld.ApiProvider/Repositories/Implementations/ProductRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PlantWorld.ApiProvider.Models;
+using PlantWorld.ApiProvider.Repositories.Interfaces;
+
+namespace PlantWorld.ApiProvider.Repositories.Implementations
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Product> AddAsync(Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return product;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return false;
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .ToListAsync();
+        }
+
+        public async Task<Product?> GetByIdAsync(int id)
+        {
+            return await _context.Products
+                .Include(p => p.Category)
+                .FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Product?> UpdateAsync(Product product)
+        {
+            var existingProduct = await _context.Products.FindAsync(product.Id);
+            if (existingProduct == null)
+            {
+                return null;
+            }
+            existingProduct.Name = product.Name;
+            existingProduct.Description = product.Description;
+            existingProduct.ImgUrl = product.ImgUrl;
+            existingProduct.Price = product.Price;
+            existingProduct.CategoryId = product.CategoryId;
+            await _context.SaveChangesAsync();
+            return existingProduct;
+
+        }
+    }
+}
