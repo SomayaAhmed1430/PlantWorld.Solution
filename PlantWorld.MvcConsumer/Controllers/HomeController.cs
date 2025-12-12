35167146ddc96fe90490e3cc413fe7cf8ef8e780
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-12 23:43:47 +0200

    Create MVC Project (Consumer)

---
+++ PlantWorld.MvcConsumer/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using PlantWorld.MvcConsumer.Models;
+using System.Diagnostics;
+
+namespace PlantWorld.MvcConsumer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
