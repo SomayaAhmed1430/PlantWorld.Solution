f7a2166948a095a98e1d39bfc11ee480b9268d21
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-17 00:20:14 +0200

    Create CategoryController (Index) | Index View | Dashboard Layout | Dashboard.css | Add FavIcon

---
+++ PlantWorld.MvcConsumer/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlantWorld.MvcConsumer.Services.Interfaces;
+
+namespace PlantWorld.MvcConsumer.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _categoryService.GetAllAsync();
+            return View(categories);
+        }
+    }
+}
