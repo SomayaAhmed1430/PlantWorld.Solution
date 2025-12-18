150afbf46e0abd5d56bb00a29e245ba39a516870
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-18 22:51:57 +0200

    Create CategoryController (Create) | Create View

--- PlantWorld.MvcConsumer/Controllers/CategoryController.cs
+++ PlantWorld.MvcConsumer/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PlantWorld.MvcConsumer.Models.CategoryDTOs;
 using PlantWorld.MvcConsumer.Services.Interfaces;
 
 namespace PlantWorld.MvcConsumer.Controllers
@@ -10,10 +11,51 @@ namespace PlantWorld.MvcConsumer.Controllers
         {
             _categoryService = categoryService;
         }
+
+        // GET: Category
         public async Task<IActionResult> Index()
         {
             var categories = await _categoryService.GetAllAsync();
             return View(categories);
+        }
+
+        // GET: Category/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        // POST: Category/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CategoryCreateDTO categoryDto, IFormFile imageFile)
+        {
+            if (!ModelState.IsValid)
+                return View(categoryDto);
+
+            // رفع الصورة
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/categories");
+
+                if (!Directory.Exists(uploadsFolder))
+                    Directory.CreateDirectory(uploadsFolder);
+
+                var fileName = Guid.NewGuid().ToString() + Path.GetExtension(imageFile.FileName);
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                using var stream = new FileStream(filePath, FileMode.Create);
+                await imageFile.CopyToAsync(stream);
+
+                // نخزن المسار
+                categoryDto.ImgUrl = "/images/categories/" + fileName;
+            }
+
+            // نبعت للـ API
+            await _categoryService.AddAsync(categoryDto);
+
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
