d895b977f2fdf381500a862e4fb096f7f55e6a7a
SomayaAhmed1430 <s.ramadan02185@dmu.edu.eg>
2025-12-25 21:57:15 +0200

    Create Checkout Controller

---
+++ PlantWorld.ApiProvider/Controllers/CheckoutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlantWorld.ApiProvider.DTOs.CheckoutDTOs;
+using PlantWorld.ApiProvider.Models;
+using PlantWorld.ApiProvider.Repositories.Interfaces;
+
+namespace PlantWorld.ApiProvider.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CheckoutController : ControllerBase
+    {
+        private readonly ICheckoutRepository _checkoutRepo;
+        private readonly IProductRepository _productRepo;
+
+        public CheckoutController(
+            ICheckoutRepository checkoutRepo,IProductRepository productRepo)
+        {
+            _checkoutRepo = checkoutRepo;
+            _productRepo = productRepo;
+        }
+    }
+}
